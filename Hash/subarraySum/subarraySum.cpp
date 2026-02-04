ded9794b924ec260d6037d484e75f089e72ad6bf
HITGerhman <2727190275@qq.com>
2026-02-04 10:34:15 +0800

    subarraySum1

--- Hash/subarraySum/subarraySum.cpp
+++ Hash/subarraySum/subarraySum.cpp
@@ -5,52 +5,61 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums,int k) {
-        unordered_map<int,int> mp;
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
         mp[0] = 1;
         int count = 0;
         int currentSum = 0;
 
-        for(int num:nums) {
-            currentSum+=num;
-            if(mp.find(currentSum-k)!=mp.end()) {
-                count+=mp[currentSum-k];
+        for (int num : nums)
+        {
+            currentSum += num;
+            if (mp.find(currentSum - k) != mp.end())
+            {
+                count += mp[currentSum - k];
             }
             mp[currentSum]++;
-
         }
         return count;
-
     }
 };
 
-int main() {
-    if(!freopen("input.txt","r",stdin))  {
+int main()
+{
+    if (!freopen("input.txt", "r", stdin))
+    {
         return 0;
     }
     string line;
     int k;
-    while(getline(cin,line)&&cin>>k) {
+    while (getline(cin, line) && cin >> k)
+    {
         string dummy;
-        getline(cin,dummy);
-        if(line.empty()) {
+        getline(cin, dummy);
+        if (line.empty())
+        {
             continue;
         }
-        for(char& c:line) {
-            if(c=='['||c==']'||c==',') {
+        for (char &c : line)
+        {
+            if (c == '[' || c == ']' || c == ',')
+            {
                 c = ' ';
             }
         }
         stringstream ss(line);
         vector<int> nums;
         int temp;
-        while(ss>>temp) {
+        while (ss >> temp)
+        {
             nums.push_back(temp);
-        }   
+        }
         Solution sol;
-        int res = sol.subarraySum(nums,k);
-        cout<<res<<endl;
+        int res = sol.subarraySum(nums, k);
+        cout << res << endl;
     }
 }
