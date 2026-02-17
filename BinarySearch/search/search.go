df3740c0ab30a7e63c18c19ae1f0145faf449a1c
HITGerhman <2727190275@qq.com>
2026-02-17 11:14:18 +0800

    search2

--- BinarySearch/search/search.go
+++ BinarySearch/search/search.go
@@ -9,24 +9,19 @@ import (
 )
 
 func search(nums []int, target int) int {
-	l, r := 0, len(nums)-1
+	n := len(nums)
+	l := 0
+	r := n - 1
 	for l <= r {
 		mid := l + (r-l)/2
 		if nums[mid] == target {
 			return mid
 		}
-		if nums[l] <= nums[mid] {
-			if nums[l] <= target && target < nums[mid] {
-				r = mid - 1
-			} else {
-				l = mid + 1
-			}
-		} else {
-			if nums[mid] < target && target <= nums[r] {
-				l = mid + 1
-			} else {
-				r = mid - 1
-			}
+		if nums[mid] < target {
+			l = mid + 1
+		}
+		if nums[mid] > target {
+			r = mid - 1
 		}
 	}
 	return -1
