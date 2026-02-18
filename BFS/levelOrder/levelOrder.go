a0031ee3e98ed4e33827200e49ea546f1a1fadd8
HITGerhman <2727190275@qq.com>
2026-02-18 15:37:46 +0800

    levelOrder

---
+++ BFS/levelOrder/levelOrder.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	res := make([][]int, 0)
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		level := make([]int, 0, levelSize)
+
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			level = append(level, node.Val)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		res = append(res, level)
+	}
+
+	return res
+}
+
+func buildTree(arr []any) *TreeNode {
+	if len(arr) == 0 || arr[0] == nil {
+		return nil
+	}
+
+	root := &TreeNode{Val: int(arr[0].(float64))}
+	queue := []*TreeNode{root}
+	i := 1
+
+	for len(queue) > 0 && i < len(arr) {
+		cur := queue[0]
+		queue = queue[1:]
+
+		if i < len(arr) && arr[i] != nil {
+			cur.Left = &TreeNode{Val: int(arr[i].(float64))}
+			queue = append(queue, cur.Left)
+		}
+		i++
+
+		if i < len(arr) && arr[i] != nil {
+			cur.Right = &TreeNode{Val: int(arr[i].(float64))}
+			queue = append(queue, cur.Right)
+		}
+		i++
+	}
+
+	return root
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+
+	in := bufio.NewScanner(f)
+	in.Scan()
+
+	var arr []any
+	_ = json.Unmarshal([]byte(in.Text()), &arr)
+	fmt.Println(levelOrder(buildTree(arr)))
+}
