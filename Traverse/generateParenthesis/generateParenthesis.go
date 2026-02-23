950794ce3e6fb94bb437d6c5a2fa38d4a5edfad7
HITGerhman <2727190275@qq.com>
2026-02-23 16:58:17 +0800

    generateParenthesis

--- Traverse/generateParenthesis/generateParenthesis.go
+++ Traverse/generateParenthesis/generateParenthesis.go
@@ -1,3 +1,37 @@
 package main
 
-func generateParenthesis()
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func generateParenthesis(n int) []string {
+	res := []string{}
+	var dfs func(l, r int, path string)
+	dfs = func(l, r int, path string) {
+		if l == n && r == n {
+			res = append(res, path)
+			return
+		}
+		if l < n {
+			dfs(l+1, r, path+"(")
+		}
+		if r < l {
+			dfs(l, r+1, path+")")
+		}
+	}
+	dfs(0, 0, "")
+	return res
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+	in := bufio.NewScanner(f)
+
+	in.Scan()
+	n, _ := strconv.Atoi(in.Text())
+	fmt.Println(generateParenthesis(n))
+}
