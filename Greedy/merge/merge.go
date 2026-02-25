2a47c5d480f3c63a5fcc8debb9b80ac1df696c69
HITGerhman <2727190275@qq.com>
2026-02-25 09:26:15 +0800

    merge2

--- Greedy/merge/merge.go
+++ Greedy/merge/merge.go
@@ -9,17 +9,21 @@ import (
 )
 
 func merge(intervals [][]int) [][]int {
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
-	})
 	if len(intervals) == 0 {
 		return nil
 	}
-	res := make([][]int, 0)
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	res := [][]int{}
 	l, r := intervals[0][0], intervals[0][1]
+
 	for i := 1; i < len(intervals); i++ {
 		if intervals[i][0] <= r {
-			r = max(r, intervals[i][1])
+			if intervals[i][1] > r {
+				r = intervals[i][1]
+			}
 		} else {
 			res = append(res, []int{l, r})
 			l, r = intervals[i][0], intervals[i][1]
