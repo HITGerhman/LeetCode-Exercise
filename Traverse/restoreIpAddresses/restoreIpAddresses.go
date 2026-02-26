d46c8f4bdba041d945cc8cff2c71760cacd337bc
HITGerhman <2727190275@qq.com>
2026-02-26 10:10:45 +0800

    restoreIpAddresses

--- Traverse/restoreIpAddresses/restoreIpAddresses.go
+++ Traverse/restoreIpAddresses/restoreIpAddresses.go
@@ -9,47 +9,53 @@ import (
 )
 
 func restoreIpAddresses(s string) []string {
+	var res []string
 	n := len(s)
 	if n < 4 || n > 12 {
 		return []string{}
 	}
 
-	res := make([]string, 0)
-	path := make([]string, 0, 4)
+	var path []string
 
 	var dfs func(start int)
 	dfs = func(start int) {
-		segments := len(path)
-		if segments == 4 {
+		if len(path) == 4 {
 			if start == n {
 				res = append(res, strings.Join(path, "."))
 			}
 			return
 		}
 
-		remainChars := n - start
-		remainSeg := 4 - segments
-		if remainChars < remainSeg || remainChars > remainSeg*3 {
+		// 剪枝：剩余字符数量必须在合理范围内（每段至少1个字符，至多3个）
+		remainChar := n - start
+		remainSeg := 4 - len(path)
+		if remainChar < remainSeg || remainChar > remainSeg*3 {
 			return
 		}
 
-		num := 0
-		for end := start; end < n && end < start+3; end++ {
-			if end > start && s[start] == '0' {
+		for i := 1; i <= 3; i++ {
+			if start+i > n {
+				break
+			}
+			sub := s[start : start+i]
+			// 检查前导0：如果长度大于1且第一位是0，则非法
+			if len(sub) > 1 && sub[0] == '0' {
+				break
+			}
+			// 检查数值是否 <= 255
+			val := 0
+			for _, c := range sub {
+				val = val*10 + int(c-'0')
+			}
+			if val > 255 {
 				break
 			}
 
-			num = num*10 + int(s[end]-'0')
-			if num > 255 {
-				break
-			}
-
-			path = append(path, s[start:end+1])
-			dfs(end + 1)
+			path = append(path, sub)
+			dfs(start + i)
 			path = path[:len(path)-1]
 		}
 	}
-
 	dfs(0)
 	return res
 }
