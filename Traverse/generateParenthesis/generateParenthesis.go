67f2167210d035e7467cc95540e7338913bae349
HITGerhman <2727190275@qq.com>
2026-02-26 09:25:12 +0800

    generateParenthesis

--- Traverse/generateParenthesis/generateParenthesis.go
+++ Traverse/generateParenthesis/generateParenthesis.go
@@ -8,26 +8,26 @@ import (
 )
 
 func generateParenthesis(n int) []string {
-	res := []string{}
-	var dfs func(l, r int, path []byte)
-	dfs = func(l, r int, path []byte) {
+	var res []string
+	var path []byte
+	var dfs func(l, r int)
+	dfs = func(l, r int) {
 		if l == n && r == n {
 			res = append(res, string(path))
 			return
 		}
 		if l < n {
 			path = append(path, '(')
-			dfs(l+1, r, path)
-			path = path[:len(path)-1] // 回溯：移除刚才添加的字符
+			dfs(l+1, r)
+			path = path[:len(path)-1]
 		}
 		if r < l {
 			path = append(path, ')')
-			dfs(l, r+1, path)
-			path = path[:len(path)-1] // 回溯
+			dfs(l, r+1)
+			path = path[:len(path)-1]
 		}
 	}
-	// 预分配容量为 2*n 的切片，避免 append 时扩容
-	dfs(0, 0, make([]byte, 0, 2*n))
+	dfs(0, 0)
 	return res
 }
 
